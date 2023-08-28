c07dc53060e099c6483155ab5848900f7b6cec30
Erick Muller <erickma1991@hotmail.com>
2023-08-28 15:47:59 +1200

    Agregando ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import logodbz from './img/logo-dbz.png';
+import ListaDeTareas from './components/listaDeTareas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-imagen-contenedor'>
+        <img 
+        src={logodbz}
+        className='logo-dbz'
+        alt='logo dragon ball Z' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>  
+        <ListaDeTareas />
+      </div>      
     </div>
   );
 }
